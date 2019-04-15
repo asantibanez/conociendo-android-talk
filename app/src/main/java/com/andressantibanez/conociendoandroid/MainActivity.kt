2e61cadcf1190721ddced7230f7e6eb26c93a7ed
asantibanez <santibanez.andres@gmail.com>
2019-04-15 14:17:45 -0500

    Threading demo with AsyncTask

--- app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
+++ app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
@@ -1,28 +1,39 @@
 package com.andressantibanez.conociendoandroid
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import java.lang.Thread.sleep
 
+@SuppressLint("StaticFieldLeak")
 class MainActivity : AppCompatActivity() {
+
+    private var message: TextView? = null
+    private var sayByeButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val message: TextView = findViewById(R.id.message)
-        val sayByeButton: Button = findViewById(R.id.sayByeButton)
+        message = findViewById(R.id.message)
+        sayByeButton = findViewById(R.id.sayByeButton)
 
-        sayByeButton.setOnClickListener {
-            Thread(Runnable {
-                sleep(3000)
-                runOnUiThread {
-                    message.text = "Say Bye!"
-                }
-            }).start()
+        sayByeButton?.setOnClickListener {
+            SayByeTask().execute()
         }
     }
 
+    inner class SayByeTask : AsyncTask<Unit, Unit, Unit>() {
+        override fun doInBackground(vararg p0: Unit) {
+            sleep(3000)
+            return
+        }
+
+        override fun onPostExecute(result: Unit) {
+            message?.text = "Say Bye!"
+        }
+    }
 }
