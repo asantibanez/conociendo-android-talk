df6a4805a6b2d3bc9269104e97d35081197e1d3a
asantibanez <santibanez.andres@gmail.com>
2019-04-15 14:23:44 -0500

    Threading demo with RxJava2

--- app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
+++ app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.andressantibanez.conociendoandroid
 
-import android.annotation.SuppressLint
-import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
-import java.lang.Thread.sleep
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-@SuppressLint("StaticFieldLeak")
 class MainActivity : AppCompatActivity() {
 
     private var message: TextView? = null
@@ -22,18 +22,14 @@ class MainActivity : AppCompatActivity() {
         sayByeButton = findViewById(R.id.sayByeButton)
 
         sayByeButton?.setOnClickListener {
-            SayByeTask().execute()
-        }
-    }
+            Observable.just(true)
+                .delay(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    message?.text = "Say Bye!"
+                }
 
-    inner class SayByeTask : AsyncTask<Unit, Unit, Unit>() {
-        override fun doInBackground(vararg p0: Unit) {
-            sleep(3000)
-            return
-        }
-
-        override fun onPostExecute(result: Unit) {
-            message?.text = "Say Bye!"
         }
     }
 }
