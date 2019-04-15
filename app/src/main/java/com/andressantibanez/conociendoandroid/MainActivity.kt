50f510ed1dd610af5f7731637beea51ebc9a7ff9
asantibanez <santibanez.andres@gmail.com>
2019-04-15 13:54:30 -0500

    Threading demo

--- app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
+++ app/src/main/java/com/andressantibanez/conociendoandroid/MainActivity.kt
@@ -2,11 +2,25 @@ package com.andressantibanez.conociendoandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val message: TextView = findViewById(R.id.message)
+        val sayByeButton: Button = findViewById(R.id.sayByeButton)
+
+        sayByeButton.setOnClickListener {
+            Thread(Runnable {
+                sleep(3000)
+                message.text = "Say Bye!"
+            }).start()
+        }
     }
+
 }
